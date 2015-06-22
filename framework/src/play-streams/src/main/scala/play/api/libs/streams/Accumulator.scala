71a8e18ab1522e5349baefbac1cd5f2323108eec
James Roper <james@jazzy.id.au>
2015-06-22 11:03:04 +1000

    Implement zero or one optimisation for accumulator

--- framework/src/play-streams/src/main/scala/play/api/libs/streams/Accumulator.scala
+++ framework/src/play-streams/src/main/scala/play/api/libs/streams/Accumulator.scala
@@ -13,37 +13,38 @@ import scala.util.{ Failure, Success }
  * This is essentially a lightweight wrapper around a Sink that gets materialised to a Future, but provides convenient
  * methods for working directly with that future as well as transforming the input.
  */
-final class Accumulator[-E, +A](sink: Sink[E, Future[A]]) {
+sealed trait Accumulator[-E, +A] {
+
+  protected def mapMaterialized[B](f: Future[A] => Future[B]): Accumulator[E, B]
 
   /**
    * Map the result of this accumulator to something else.
    */
   def map[B](f: A => B)(implicit executor: ExecutionContext): Accumulator[E, B] =
-    new Accumulator(sink.mapMaterializedValue(_.map(f)))
+    mapMaterialized(_.map(f))
 
   /**
    * Map the result of this accumulator to a future of something else.
    */
   def mapFuture[B](f: A => Future[B])(implicit executor: ExecutionContext): Accumulator[E, B] =
-    new Accumulator(sink.mapMaterializedValue(_.flatMap(f)))
+    mapMaterialized(_.flatMap(f))
 
   /**
    * Recover from errors encountered by this accumulator.
    */
   def recover[B >: A](pf: PartialFunction[Throwable, B])(implicit executor: ExecutionContext): Accumulator[E, B] =
-    new Accumulator(sink.mapMaterializedValue(_.recover(pf)))
+    mapMaterialized(_.recover(pf))
 
   /**
    * Recover from errors encountered by this accumulator.
    */
   def recoverWith[B >: A](pf: PartialFunction[Throwable, Future[B]])(implicit executor: ExecutionContext): Accumulator[E, B] =
-    new Accumulator(sink.mapMaterializedValue(_.recoverWith(pf)))
+    mapMaterialized(_.recoverWith(pf))
 
   /**
    * Return a new accumulator that first feeds the input through the given flow before it goes through this accumulator.
    */
-  def through[F](flow: Flow[F, E, _]): Accumulator[F, A] =
-    new Accumulator(flow.toMat(sink)(Keep.right))
+  def through[F](flow: Flow[F, E, _]): Accumulator[F, A]
 
   /**
    * Right associative operator alias for through.
@@ -56,22 +57,18 @@ final class Accumulator[-E, +A](sink: Sink[E, Future[A]]) {
    *   val stringAccumulator = toInt ~>: intAccumulator
    * }}}
    */
-  def ~>:[F](flow: Flow[F, E, _]): Accumulator[F, A] = through(flow)
+  def ~>:[F](flow: Flow[F, E, _]): Accumulator[F, A] =
+    through(flow)
 
   /**
    * Run this accumulator by feeding in the given source.
    */
-  def run(source: Source[E, _])(implicit materializer: FlowMaterializer): Future[A] = {
-    source.toMat(sink)(Keep.right).run()
-  }
+  def run(source: Source[E, _])(implicit materializer: FlowMaterializer): Future[A]
 
   /**
    * Run this accumulator by feeding a completed source into it.
    */
-  def run()(implicit materializer: FlowMaterializer): Future[A] = {
-    run(Source.empty)
-  }
-
+  def run()(implicit materializer: FlowMaterializer): Future[A]
   /**
    * Right associative operator alias for run.
    *
@@ -83,42 +80,103 @@ final class Accumulator[-E, +A](sink: Sink[E, Future[A]]) {
    *   val intFuture = source ~>: intAccumulator
    * }}}
    */
-  def ~>:(source: Source[E, _])(implicit materializer: FlowMaterializer): Future[A] = run(source)
+  def ~>:(source: Source[E, _])(implicit materializer: FlowMaterializer): Future[A] =
+    run(source)
+
+  /**
+   * Run this accumulator by feeding a single element into it.
+   */
+  def run(e: E)(implicit materializer: FlowMaterializer): Future[A]
 
   /**
    * Convert this accumulator to a Sink that gets materialised to a Future.
    */
-  def toSink: Sink[E, Future[A]] = sink
+  def toSink: Sink[E, Future[A]]
 }
 
-object Accumulator {
+private class ImmediateAccumulator[-E, +A](many: Sink[E, Future[A]], zeroOrOne: Option[Option[E] => Future[A]]) extends Accumulator[E, A] {
+
+  protected def mapMaterialized[B](f: Future[A] => Future[B]): Accumulator[E, B] =
+    new ImmediateAccumulator(
+      many.mapMaterializedValue(f),
+      zeroOrOne match {
+        case Some(zo) => Some(zo.andThen(f))
+        case None => None
+      }
+    )
 
   /**
-   * Create a new accumulator from the given Sink.
+   * Return a new accumulator that first feeds the input through the given flow before it goes through this accumulator.
    */
-  def apply[E, A](sink: Sink[E, Future[A]]): Accumulator[E, A] = new Accumulator(sink)
+  def through[F](flow: Flow[F, E, _]): Accumulator[F, A] =
+    new ImmediateAccumulator(flow.toMat(many)(Keep.right), None)
 
   /**
-   * Create a done accumulator.
-   *
-   * The underlying sink will cancel as soon as its onSubscribe method is called, and the materialized value will be
-   * an immediately available future of `a`.
+   * Run this accumulator by feeding in the given source.
    */
-  def done[A](a: A): Accumulator[Any, A] = new Accumulator(Sink.cancelled[Any].mapMaterializedValue(_ => Future.successful(a)))
+  def run(source: Source[E, _])(implicit materializer: FlowMaterializer): Future[A] = {
+    source.toMat(many)(Keep.right).run()
+  }
 
   /**
-   * Create a done accumulator.
-   *
-   * The underlying sink will cancel as soon as its onSubscribe method is called, and the materialized value will be
-   * the passed in future.
+   * Run this accumulator by feeding a completed source into it.
    */
-  def done[A](a: Future[A]): Accumulator[Any, A] = new Accumulator(Sink.cancelled[Any].mapMaterializedValue(_ => a))
+  def run()(implicit materializer: FlowMaterializer): Future[A] =
+    zeroOrOne match {
+      case Some(f) => f(None)
+      case None => run(Source.empty)
+    }
 
   /**
-   * Flatten a future of an accumulator to an accumulator.
+   * Run this accumulator by feeding a single element into it.
    */
-  def flatten[E, A](future: Future[Accumulator[E, A]])(implicit materializer: FlowMaterializer): Accumulator[E, A] = {
-    import play.api.libs.iteratee.Execution.Implicits.trampoline
+  def run(e: E)(implicit materializer: FlowMaterializer): Future[A] =
+    zeroOrOne match {
+      case Some(f) => f(Some(e))
+      case None => run(Source.single(e))
+    }
+
+  /**
+   * Convert this accumulator to a Sink that gets materialised to a Future.
+   */
+  def toSink: Sink[E, Future[A]] = many
+}
+
+private class FutureAccumulator[-E, +A](future: Future[Accumulator[E, A]], mat: FlowMaterializer) extends Accumulator[E, A] {
+  import play.api.libs.iteratee.Execution.Implicits.trampoline
+
+  protected def mapMaterialized[B](f: (Future[A]) => Future[B]) =
+    new FutureAccumulator(future.map(_.mapMaterialized(f)), mat)
+
+  /**
+   * Return a new accumulator that first feeds the input through the given flow before it goes through this accumulator.
+   */
+  def through[F](flow: Flow[F, E, _]) =
+    new FutureAccumulator(future.map(_.through(flow)), mat)
+
+  /**
+   * Run this accumulator by feeding in the given source.
+   */
+  def run(source: Source[E, _])(implicit materializer: FlowMaterializer) =
+    future.flatMap(_.run(source))
+
+  /**
+   * Run this accumulator by feeding a completed source into it.
+   */
+  def run()(implicit materializer: FlowMaterializer) =
+    future.flatMap(_.run())
+
+  /**
+   * Run this accumulator by feeding a single element into it.
+   */
+  def run(e: E)(implicit materializer: FlowMaterializer) =
+    future.flatMap(_.run(e))
+
+  /**
+   * Convert this accumulator to a Sink that gets materialised to a Future.
+   */
+  def toSink = {
+    implicit val mat = this.mat
 
     // Ideally, we'd use the following code, except due to akka streams bugs...
     // new Accumulator(Sink.publisher[E].mapMaterializedValue { publisher =>
@@ -153,7 +211,45 @@ object Accumulator {
       def onNext(t: E) = subscriber.onNext(t)
     })
 
-    new Accumulator(sink.mapMaterializedValue(_ => result.future))
+    sink.mapMaterializedValue(_ => result.future)
   }
+}
+
+object Accumulator {
+
+  /**
+   * Create a new accumulator from the given Sink.
+   */
+  def apply[E, A](sink: Sink[E, Future[A]]): Accumulator[E, A] = new ImmediateAccumulator(sink, None)
+
+  /**
+   * Create a done accumulator.
+   *
+   * The underlying sink will cancel as soon as its onSubscribe method is called, and the materialized value will be
+   * an immediately available future of `a`.
+   */
+  def done[A](a: A): Accumulator[Any, A] =
+    new ImmediateAccumulator(
+      Sink.cancelled[Any].mapMaterializedValue(_ => Future.successful(a)),
+      Some(_ => Future.successful(a))
+    )
+
+  /**
+   * Create a done accumulator.
+   *
+   * The underlying sink will cancel as soon as its onSubscribe method is called, and the materialized value will be
+   * the passed in future.
+   */
+  def done[A](a: Future[A]): Accumulator[Any, A] =
+    new ImmediateAccumulator(
+      Sink.cancelled[Any].mapMaterializedValue(_ => a),
+      Some(_ => a)
+    )
+
+  /**
+   * Flatten a future of an accumulator to an accumulator.
+   */
+  def flatten[E, A](future: Future[Accumulator[E, A]])(implicit materializer: FlowMaterializer): Accumulator[E, A] =
+    new FutureAccumulator(future, materializer)
 
 }
